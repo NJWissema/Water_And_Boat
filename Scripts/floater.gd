682badb1c332594b4b567e9512f8b26df92c62af
Nyx <nj.wissema@gmail.com>
2025-04-02 09:50:41 +0200

    blooop

--- Scripts/floater.gd
+++ Scripts/floater.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 
+@onready var arrow: Node3D = $Arrow
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,4 +9,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
+
+
+#Approximate the height of the water
+func wave_height_calc():
 	pass
